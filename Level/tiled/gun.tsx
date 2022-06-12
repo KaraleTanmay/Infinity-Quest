04ddc2c7e8d580c6f60a57084d2e546d18d573f0
KaraleTanmay <107341046+KaraleTanmay@users.noreply.github.com>
2022-06-12 23:14:08 +0530

    Delete Level directory

--- Level/tiled/gun.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="gun" tilewidth="80" tileheight="40" tilecount="1" columns="1">
- <tileoffset x="0" y="-20"/>
- <image source="../../Graphics/level images/gun.png" width="80" height="40"/>
-</tileset>
--- Level/tiled/slicer.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="slicer" tilewidth="80" tileheight="80" tilecount="1" columns="1">
- <tileoffset x="0" y="40"/>
- <image source="../../Graphics/level images/slicer.png" width="80" height="80"/>
-</tileset>
